b26ccc2278c6bab4e67f761f55e773966708aa44
Beni <braunger@zib.de>
2025-08-27 17:28:13 +0200

    first working draft

--- packet_redirection/packet_redirect.bpf.c
+++ packet_redirection/packet_redirect.bpf.c
@@ -1,13 +1,14 @@
 #include "vmlinux.h"
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
+#include "xx_hash.h"
 
 #define ETH_P_IP 0x0800 /* Internet Protocol packet	*/
 #define TC_ACT_OK 0
 #define TC_ACT_REDIRECT 7
 
 // the interface we will redirect to.
-#define TARGET_INTF 3
+#define TARGET_INTF 4
 // the destination IP that determines if we will redirect the packet.
 #define DEST_IP 0x0a00020b  // 10.0.2.11
 
@@ -19,9 +20,9 @@ int egress_redirect(struct __sk_buff *ctx) {
     void *data = (void *)(__u64)(ctx->data);
     struct ethhdr *eth;
     struct iphdr *ipv4;
-    int ret;
+    int ret = TC_ACT_OK;
 
-    bpf_printk("redirect: handling packet\n");
+    bpf_printk("redirect: handling a packet\n");
 
     // bounds check for verifier, packet's data must be at least as large
     // as an ethernet header and the non-variable portion of the IPv4 header.
@@ -31,22 +32,27 @@ int egress_redirect(struct __sk_buff *ctx) {
     eth = data;
     ipv4 = data + sizeof(struct ethhdr);
 
-    bpf_printk("redirect: checking ethernet header for IPv4 proto: %x\n", bpf_ntohs(eth->h_proto));
+    bpf_printk("redirect: checking ethernet header for IPv4 proto. result: %x\n", bpf_ntohs(eth->h_proto));
     if (bpf_ntohs(eth->h_proto) != ETH_P_IP) return TC_ACT_OK;
 
     bpf_printk("redirect: checking destination address is 10.0.2.11\n");
     if (bpf_ntohl(ipv4->daddr) != DEST_IP) return TC_ACT_OK;
 
-    bpf_printk("redirect: rewriting destination MAC\n");
-    eth->h_dest[0] = 0x02;
-    eth->h_dest[1] = 0x00;
-    eth->h_dest[2] = 0x00;
-    eth->h_dest[3] = 0x00;
-    eth->h_dest[4] = 0x00;
-    eth->h_dest[5] = 0x00;
+    //bpf_printk("redirect: rewriting destination MAC\n");
+    //eth->h_dest[0] = 0x02;
+    //eth->h_dest[1] = 0x00;
+    //eth->h_dest[2] = 0x00;
+    //eth->h_dest[3] = 0x00;
+    //eth->h_dest[4] = 0x00;
+    //eth->h_dest[5] = 0x00;
 
-    bpf_printk("redirect: performing redirect\n");
-    ret = bpf_redirect(TARGET_INTF, 0);
+    __u32 key = xxhash32((const char*)ipv4, sizeof(struct iphdr), 0) % 2;
+    if (key != 0) { 
+        bpf_printk("redirect: performing redirect\n");
+        ret = bpf_redirect(TARGET_INTF, 0);
+    } else {
+        bpf_printk("redirect: NOT performing redirect\n");
+    }
 
     bpf_printk("redirect: result: %d\n", ret);
     return ret;
